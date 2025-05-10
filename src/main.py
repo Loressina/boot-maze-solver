b719a0e08d3f788f691541b8dba5a751f27b4ca5
Loressina <loressina@posteo.de>
2025-05-10 21:49:37 +0200

    add points and lines

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
     win = Window(800, 600)
+
+    a = Point(100, 50)
+    b = Point(600, 400)
+    line = Line(a, b)
+    win.draw_line(line, "black")
+
     win.wait_for_close()
 
 
